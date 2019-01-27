49d1b243c3649a0850f509a1b1447832e22e3841
cemondr <cemconder@gmail.com>
2019-01-26 23:46:16 -0800

    minor fix

--- bots/pdxgroup4real/robot.js
+++ bots/pdxgroup4real/robot.js
@@ -33,28 +33,7 @@ class MyRobot extends BCAbstractRobot {
             const choice = choices[Math.floor(Math.random()*choices.length)]
             return this.move(...choice);
         }
-        else if (this.me.unit === SPECS.PREACHER) {
-            this.log("PREACHER health: " + this.me.health);
-            var visible = this.getVisibleRobots();
-                //get attacable robot...
-            var r;
-            for(r in visible)
-            {
-                if(this.isVisible(visible[r]))
-                {
-                    var dist = this.squareDistance(visible[r]);
 
-                    if(this.me.team != visible[r].team && dist <= 16)
-                    {
-                        this.log("Attacking: " + visible[r].id);
-                        return this.attack(visible[r].x - this.me.x, visible[r].y - this.me.y);
-                    }
-                }
-            }
-            const choices = [[0,-1], [1, -1], [1, 0], [1, 1], [0, 1], [-1, 1], [-1, 0], [-1, -1]];
-            const choice = choices[Math.floor(Math.random()*choices.length)];
-            return this.move(...choice);
-        }
         else if (this.me.unit === SPECS.CASTLE) {
             if (this.karbonite > 55) {    //for first 10 steps
                 const choices = [[0,-1], [1, -1], [1, 0], [1, 1], [0, 1], [-1, 1], [-1, 0], [-1, -1]];
@@ -92,7 +71,6 @@ class MyRobot extends BCAbstractRobot {
                     
                     /*THE REPLAY ON BATTLECODE SITE DOESN'T DISPLAY KARONITE CORRECTLY SO
                     THIS IS A GOOD WAY TO TEST THE PILGRIM KARBONITE LEVEL
-
                     this.log("My Karbonite: " + this.me.karbonite);
                     this.log("--------------------I AM MINING---------------------------");
                     */
@@ -100,7 +78,7 @@ class MyRobot extends BCAbstractRobot {
                 }
                 
             }
-            else if (this.me.karbonite <= 20 && this.me.karbonite >=10){
+            else if (this.me.karbonite <= 20 && this.me.karbonite >=10){ //minor fix
 
                 var visibleRobots = this.getVisibleRobots();
                 var i;
@@ -108,19 +86,19 @@ class MyRobot extends BCAbstractRobot {
                 var dumpDistance;
                 
                 for (i in visibleRobots){
-                    
+                
                     if (this.isVisible(visibleRobots[i])){
 
                         if (visibleRobots[i].team === this.me.team && visibleRobots[i].unit === SPECS.CASTLE){
-
-                            dumpDistance = this.squareDistance(visibleRobots[i],this.me);
                             
+                            dumpDistance = this.squareDistance(visibleRobots[i],this.me);
+
+                            if (dumpDistance <= 2){
+
                                 targetDump = visibleRobots[i];
                                 
-                                /* ON REPLAY, THE TEAM KARBONITE VALUES ARE NOT RELIABLE 
                                 this.log("Team Karbonite: " + this.karbonite);
                                 this.log("---------------DUMPING CARB------------")
-                                */
 
                                 return this.give(targetDump.x - this.me.x,targetDump.y - this.me.y,this.me.karbonite,
                                 this.me.fuel);
@@ -136,6 +114,8 @@ class MyRobot extends BCAbstractRobot {
         }
 
     }
+
+
     /* Returns the closest karbonite location from the current robot  */
     retClosestKarbLocation(robot){
         let destdistance = 6000000;
