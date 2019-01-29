aaa15b5298b11dd557c43f21dbc9e1f63f68b232
Jordan Miller <jordanmiller@Jordans-MacBook-Pro-2.local>
2019-01-29 12:18:15 -0800

    added prophet attack code

--- bots/pdxgroup4real/robot.js
+++ bots/pdxgroup4real/robot.js
@@ -1,15 +1,49 @@
-// First Commit
+
 import {BCAbstractRobot, SPECS} from 'battlecode';
 
 var step = -1;
 
-var CRUSADER_ATK_RANGE = 16;
+var CRUSADER_ATK_MIN = 1;
+var CRUSADER_ATK_MAX = 16;
+var PROPHET_ATK_MIN = 16;
+var PROPHET_ATK_MAX = 64;
 
 class MyRobot extends BCAbstractRobot {
+
     turn() {
         step++;
 
+        var MAP = this.map;
+
         if (this.me.unit === SPECS.CRUSADER) {
+            var visible = this.getVisibleRobots();
+
+            var i;
+            for(i in visible)
+            {
+                if(this.isVisible(visible[i]))
+                {
+            
+                    var dist = (visible[i].x - this.me.x)*(visible[i].x - this.me.x) 
+                        + (visible[i].y - this.me.y)*(visible[i].y - this.me.y);
+                    
+
+                    // if target in range, attack
+                    if(this.me.team != visible[i].team && dist <= CRUSADER_ATK_MAX && dist >= CRUSADER_ATK_MIN)
+                    {
+                        this.log("Attacking: " + visible[i].id);
+                        return this.attack(visible[i].x - this.me.x, visible[i].y - this.me.y);
+                    }
+                }
+            }
+
+            // no visible enemies or enemies not in range, move
+            const choices = [[0,-2], [2, -2], [2, 0], [2, 2], [0, 2], [-2, 2], [-2, 0], [-2, -2]];
+            const choice = choices[Math.floor(Math.random()*choices.length)]
+            return this.move(...choice);
+        }
+
+        else if (this.me.unit === SPECS.PROPHET) {
             var visible = this.getVisibleRobots();
 
             var i;
@@ -20,7 +54,7 @@ class MyRobot extends BCAbstractRobot {
                     var dist = (visible[i].x - this.me.x)*(visible[i].x - this.me.x) 
                         + (visible[i].y - this.me.y)*(visible[i].y - this.me.y);
 
-                    if(this.me.team != visible[i].team && dist <= CRUSADER_ATK_RANGE)
+                    if(this.me.team != visible[i].team && dist <= PROPHET_ATK_MAX && dist >= PROPHET_ATK_MIN)
                     {
                         this.log("Attacking: " + visible[i].id);
                         return this.attack(visible[i].x - this.me.x, visible[i].y - this.me.y);
@@ -28,21 +62,33 @@ class MyRobot extends BCAbstractRobot {
                 }
             }
 
-            // this.log("Crusader health: " + this.me.health);
-            const choices = [[0,-2], [2, -2], [2, 0], [2, 2], [0, 2], [-2, 2], [-2, 0], [-2, -2]];
+            const choices = [[0,-1], [1, -1], [1, 0], [1, 1], [0, 1], [-1, 1], [-1, 0], [-1, -1]];
             const choice = choices[Math.floor(Math.random()*choices.length)]
             return this.move(...choice);
         }
 
         else if (this.me.unit === SPECS.CASTLE) {
-            if (step % 10 === 0) {    //for first 10 steps
+            if(this.me.turn % 10 === 0)
+            {
                 const choices = [[0,-1], [1, -1], [1, 0], [1, 1], [0, 1], [-1, 1], [-1, 0], [-1, -1]];
                 const choice = choices[Math.floor(Math.random()*choices.length)]
 
                 this.log("Building a crusader at " + (this.me.x+choice[0]) + ", " + (this.me.y+choice[1]));
                 return this.buildUnit(SPECS.CRUSADER, choice[0], choice[1]);
-            } else {
-                return // this.log("Castle health: " + this.me.health);
+            }
+            else if(this.me.turn % 25 === 0)
+            {
+                const choices = [[0,-1], [1, -1], [1, 0], [1, 1], [0, 1], [-1, 1], [-1, 0], [-1, -1]];
+                const choice = choices[Math.floor(Math.random()*choices.length)]
+                
+                this.log("Building a prophet at " + (this.me.x+choice[0]) + ", " + (this.me.y+choice[1]));
+                return this.buildUnit(SPECS.PROPHET, choice[0], choice[1]);
+
+            }
+            else {
+                return 
+                // this.log("Castle health: " + this.me.health);
+
             }
         }
 
