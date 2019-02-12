e76b7e0bea3b0eb117808992465d92eb9d08268d
cemondr <cemconder@gmail.com>
2019-02-12 00:17:50 -0800

    mining modularized & per castle unit building

---
+++ bots/pdxgroup4real/mining.js
@@ -0,0 +1,93 @@
+import {BCAbstractRobot, SPECS} from 'battlecode';
+
+
+const mining = {};
+
+mining.findClosestResource = (robot, minemap) => {
+
+    let destdistance = 6000000;
+    let destlocation = null;
+    var map = minemap;
+    const maplength = map.length;
+    for (let y = 0; y <maplength; y++){
+        for (let x = 0; x < maplength; x++){
+            if(map[y][x]){
+                var currentDistance = mining.squareDistance({x,y},robot);
+                if(currentDistance < destdistance){
+
+                    destdistance = currentDistance;
+                    destlocation = {x,y};
+                    
+                }
+            }
+        }
+    }
+    return destlocation;
+}
+
+mining.squareDistance = (destination,start) => {
+
+    return Math.pow((destination.x - start.x),2) + Math.pow((destination.y - start.y),2);
+
+}
+
+mining.checkIfResourcesFull = (robot,karblimit,fueLimit) => {
+
+    if (robot.me.karbonite >= karblimit || robot.me.fuel >= fueLimit){
+
+        return true;
+    } 
+
+    return false;
+}
+
+mining.returnTargetDump = (robot) =>{
+
+    var visibleRobots = robot.getVisibleRobots();
+    var i;
+    var targetDump = null;
+    var dumpDistance;
+
+    for (i in visibleRobots){
+
+        if(robot.isVisible(visibleRobots[i])){
+
+            if (visibleRobots[i].team === robot.me.team && visibleRobots[i].unit === SPECS.CASTLE){
+
+                dumpDistance = mining.squareDistance(visibleRobots[i],robot.me);
+
+                if (dumpDistance <= 2){
+
+                    targetDump = visibleRobots[i];
+                    return targetDump;
+                }
+            }
+
+        }
+    }
+    return targetDump;
+}
+
+mining.readyToMine = (robot,karbLoc,fueLoc) => {
+
+    if (mining.squareDistance(fueLoc,robot.me) === 0){
+
+        if (robot.me.fuel < 50){
+
+            robot.log("----------------- I AM MINING FUEL-----------------------");
+            return true;
+        }
+    }
+    else if (mining.squareDistance(karbLoc,robot.me)=== 0){
+
+        if (robot.me.karbonite < 10){
+
+            robot.log("--------------------I AM MINING KARB---------------------------");  
+            return true;
+        }
+    }
+
+    return false;
+}
+
+export default mining;
--- bots/pdxgroup4real/robot.js
+++ bots/pdxgroup4real/robot.js
@@ -1,5 +1,7 @@
 
 import {BCAbstractRobot, SPECS} from 'battlecode';
+import mining from './mining.js'
+import unitbuilding from './unitbuilding.js'
 
 var step = -1;
 
@@ -9,6 +11,11 @@ var PROPHET_ATK_MIN = 16;
 var PROPHET_ATK_MAX = 64;
 
 class MyRobot extends BCAbstractRobot {
+
+    constructor(){
+        super()
+        this.unitCountMap = [0,0,0,0,0,0];
+    }
 
     turn() {
         step++;
@@ -43,41 +50,45 @@ class MyRobot extends BCAbstractRobot {
 
         //CASTLE
         else if (this.me.unit === SPECS.CASTLE) {
-            // build preacher and prohpet first
-            // keep trying to build for the first 10 turns
-            if (this.me.turn < 10 && this.karbonite > 70) {    //This code is building the unit preacher...
-                this.log("Building a preacher at " + (this.me.x+1) + ", " + (this.me.y+1));
-                return this.buildUnit(SPECS.PREACHER, 1, 1);
+
+            var unitMaps = unitbuilding.buildUnitMap(this);
+
+            //this.log("PILGRIM: "+ unitMaps[2])
+
+            if (unitMaps[2]>this.unitCountMap[2]){
+                this.unitCountMap[2]++;
             }
-            else if(this.me.turn < 10 && this.karbonite > 60)
-            {
-                const choices = [[0,-1], [1, -1], [1, 0], [1, 1], [0, 1], [-1, 1], [-1, 0], [-1, -1]];
-                const choice = choices[Math.floor(Math.random()*choices.length)]
+            else if(unitMaps[3]>this.unitCountMap[3]){
+                this.unitCountMap[3]++;
+            }
+            else if(unitMaps[4]> this.unitCountMap[4]){
+                this.unitCountMap[4]++;
+            }
+            else if(unitMaps[5]> this.unitCountMap[5]){
+                this.unitCountMap[5]++;
+            }
+            const choices = [[0,-1], [1, -1], [1, 0], [1, 1], [0, 1], [-1, 1], [-1, 0], [-1, -1]];
+            const choice = choices[Math.floor(Math.random()*choices.length)]
+
+            if(this.unitCountMap[2]< 2){
+
+                this.log("Building a pilgrim at " + (this.me.x+choice[0]) + ", " + (this.me.y+choice[1]));
+                return this.buildUnit(SPECS.PILGRIM, choice[0], choice[1]);
+            }
+            else if(this.unitCountMap[3]< 2){
+
+                this.log("Building a crusader at " + (this.me.x+choice[0]) + ", " + (this.me.y+choice[1]));
+                return this.buildUnit(SPECS.CRUSADER, choice[0], choice[1]);
+            }
+            else if (this.unitCountMap[4]< 1 ){
                 
                 this.log("Building a prophet at " + (this.me.x+choice[0]) + ", " + (this.me.y+choice[1]));
                 return this.buildUnit(SPECS.PROPHET, choice[0], choice[1]);
 
             }
-
-            // keep flows of crusaders after 10 turns
-            else if (this.karbonite >= 15 && this.me.turn % 10 === 0) {    
-                const choices = [[0,-1], [1, -1], [1, 0], [1, 1], [0, 1], [-1, 1], [-1, 0], [-1, -1]];
-                const choice = choices[Math.floor(Math.random()*choices.length)]
-
-                this.log("Building a crusader at " + (this.me.x+choice[0]) + ", " + (this.me.y+choice[1]));
-                return this.buildUnit(SPECS.CRUSADER, choice[0], choice[1]);
-            }
-            else if (this.karbonite >= 15 && this.me.turn < 10 ){
-
-                const choices = [[0,-1], [1, -1], [1, 0], [1, 1], [0, 1], [-1, 1], [-1, 0], [-1, -1]];
-                const choice = choices[Math.floor(Math.random()*choices.length)]
-
-                this.log("Building a pilgrim at " + (this.me.x+choice[0]) + ", " + (this.me.y+choice[1]));
-                return this.buildUnit(SPECS.PILGRIM, choice[0], choice[1]);
-                
-            } 
-            else {
-                return // this.log("Castle health: " + this.me.health);
+            else if (this.unitCountMap[5]<1){
+                this.log("Building a preacher at " + (this.me.x+choice[0]) + ", " + (this.me.y+choice[1]));
+                return this.buildUnit(SPECS.PREACHER, choice[0], choice[1]);
             }
         }
 
@@ -133,111 +144,33 @@ class MyRobot extends BCAbstractRobot {
         else if (this.me.unit === SPECS.PILGRIM){
 
             //check if there is resource to dump
-            if ((this.me.karbonite <= 20 && this.me.karbonite >=10) ||  (this.me.fuel<=100 && this.me.fuel >= 50)){ //minor fix
+            if (mining.checkIfResourcesFull(this,10,50)){ //minor fix
+                var targetDump = mining.returnTargetDump(this);
+                if(targetDump){
 
-                var visibleRobots = this.getVisibleRobots();
-                var i;
-                var targetDump;
-                var dumpDistance;
-            
-                for (i in visibleRobots){
-            
-                    if (this.isVisible(visibleRobots[i])){
+                    this.log("Team Karbonite: " + this.karbonite);
+                    this.log("---------------DUMPING RESOURCES------------------------");
 
-                        if (visibleRobots[i].team === this.me.team && visibleRobots[i].unit === SPECS.CASTLE){
-                        
-                            dumpDistance = this.squareDistance(visibleRobots[i],this.me);
-
-                            if (dumpDistance <= 2){
-
-                                targetDump = visibleRobots[i];
-                                
-                                this.log("Team Karbonite: " + this.karbonite);
-                                this.log("---------------DUMPING RESOURCES------------------------");
-
-                                return this.give(targetDump.x - this.me.x,targetDump.y - this.me.y,this.me.karbonite,
-                                this.me.fuel);
-                            }
-                        }
-                    }
+                    return this.give(targetDump.x - this.me.x,targetDump.y - this.me.y,this.me.karbonite,this.me.fuel);                
                 }
             }
             //check if resource locations exist, if so mine
-            var karblocation = this.retClosestKarbLocation(this.me);
-            var fuellocation = this.retClosestFuelLocation(this.me);
+            var karblocation = mining.findClosestResource(this.me,this.karbonite_map);
+            var fuellocation = mining.findClosestResource(this.me,this.fuel_map);
 
             if (fuellocation || karblocation){
 
-                if (this.squareDistance(fuellocation,this.me) === 0){
+                if(mining.readyToMine(this,karblocation,fuellocation)){
 
-                    if (this.fuel < 300 && this.me.fuel < 50){
-
-                        this.log("----------------- I AM MINING FUEL-----------------------");
-                        return this.mine();
-                    }
-                }
-                else if (this.squareDistance(karblocation,this.me)=== 0){
-
-                    if (this.karbonite< 500 && this.me.karbonite < 10){
-
-                        this.log("--------------------I AM MINING KARB---------------------------");                    
-                        return this.mine();
-                    }
+                    //this.log("-----------------------------I'm MINING------------------------------");
+                    return this.mine();
                 }
             }
-            
             const choices = [[0,-1], [1, -1], [1, 0], [1, 1], [0, 1], [-1, 1], [-1, 0], [-1, -1]];
             const choice = choices[Math.floor(Math.random()*choices.length)]
             return this.move(...choice);
         }
 
-    }
-
-
-    /* Returns the closest karbonite location from the current robot  */
-    retClosestKarbLocation(robot){
-        let destdistance = 6000000;
-        let destlocation = null;
-        var map = this.karbonite_map;
-        const maplength = map.length; 
-
-        for(let y = 0; y<maplength; y++){   // all 2D grid array maps structured ass arr[y][x]
-            for (let x = 0; x<maplength; x++){
-                if (map[y][x]){
-                    
-                    var currentDistance = this.squareDistance({x,y},robot);
-
-                    if (currentDistance< destdistance)
-                    {
-                        destdistance = currentDistance;
-                        destlocation = {x,y};
-                    }
-                }
-            }
-        }
-        return destlocation;
-    }
-
-    retClosestFuelLocation(robot){
-        let destdistance = 6000000;
-        let destlocation = null;
-        var map = this.fuel_map;
-        const maplength = map.length;
-
-        for (let y = 0; y <maplength; y++){
-            for (let x = 0; x < maplength; x++){
-                if(map[y][x]){
-                    var currentDistance = this.squareDistance({x,y},robot);
-
-                    if(currentDistance < destdistance){
-                        destdistance = currentDistance;
-                        destlocation = {x,y};
-                    }
-                }
-            }
-        }
-
-        return destlocation;
     }
 
     squareDistance(destination, start){
